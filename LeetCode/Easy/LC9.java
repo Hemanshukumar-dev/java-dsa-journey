4654da771889dec40dcef20b143970893b23d13a
Hemanshukumar-dev <hemanshukumar.dev@gmail.com>
2026-02-05 22:59:24 +0530

    Palindrome number

---
+++ LeetCode/Easy/LC9.java
@@ -0,0 +1,23 @@
+//Palindrome Number
+//Reverse number approach
+//digit extraction
+
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        int rev=0 ;
+        int digit = x;
+        while(x > 0){
+            
+            int num = x % 10;
+
+             rev = rev * 10 + num;
+             x = x / 10;
+        } 
+
+        return rev == digit;
+    }
+}
